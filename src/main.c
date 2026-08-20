1ed958880593cafba5e9d24067fb47c790b91968
Carlos Murilo de O. C. F. <murilojagger@gmail.com>
2026-08-20 11:19:20 -0300

    feat: add exit command and strip trailing newline

--- src/main.c
+++ src/main.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_LINE 1024
 
 int main(void) {
-  char linha[MAX_LINE];
+  char line[MAX_LINE];
 
   while (1) {
     printf("processflow> ");
 
-    char *resultado = fgets(linha, sizeof(linha), stdin);
+    char *result = fgets(line, sizeof(line), stdin);
 
-    if (resultado == NULL) {
+    if (result == NULL) {
       break;
     }
 
-    printf("%s", linha);
+    line[strcspn(line, "\n")] = '\0';
+    if (strcmp(line, "exit") == 0) {
+      break;
+    }
+    printf("%s\n", line);
   }
 }
